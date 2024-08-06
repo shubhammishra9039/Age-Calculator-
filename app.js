1a87921eda857c0810c9b42839a8ed2d8cbf9da6
Shubham Mishra <shubhammishra6595@gmail.com>
2024-08-06 20:44:46 +0530

    App

--- app.js
+++ app.js
@@ -1,44 +1,35 @@
 var showyear = document.querySelector("#showyear");
 var showmonth = document.querySelector("#showmonth");
 var showday = document.querySelector("#showday");
-let year = 00,
-  month = 00,
-  day = 00;
+let year = 0,
+  month = 0,
+  day = 0;
 
 function AgeCalculate() {
   var dob = document.getElementById("date").value;
   var userdob = new Date(dob);
   var today = new Date();
 
-  if (today.getFullYear() < userdob.getFullYear()) {
-    alert("Pleased Enter Correct Date of Broth");
-  } else if (
-    today.getFullYear() == userdob.getFullYear() &&
-    today.getMonth() >= userdob.getMonth() &&
-    today.getDate() >= userdob.getDate()
-  ) {
-    year = today.getFullYear() - userdob.getFullYear();
-    month =
-      today.getMonth() > userdob.getMonth()
-        ? today.getMonth() - userdob.getMonth()
-        : userdob.getMonth() - today.getMonth();
-    day =
-      today.getDate() > userdob.getDate()
-        ? today.getDate() - userdob.getDate()
-        : userdob.getDate() - today.getDate();
-  } else if (today.getFullYear() > userdob.getFullYear()) {
-    year = today.getFullYear() - userdob.getFullYear();
-    month =
-      today.getMonth() > userdob.getMonth()
-        ? today.getMonth() - userdob.getMonth() - 1
-        : userdob.getMonth() - today.getMonth() - 1;
-    day =
-      today.getDate() > userdob.getDate()
-        ? today.getDate() - userdob.getDate()
-        : userdob.getDate() - today.getDate();
-  } else {
-    alert("Pleased Enter Correct Date of Broth");
+  if (today < userdob) {
+    alert("Please Enter a Correct Date of Birth");
+    return;
   }
+
+  year = today.getFullYear() - userdob.getFullYear();
+  month = today.getMonth() - userdob.getMonth();
+  day = today.getDate() - userdob.getDate();
+
+  if (month < 0) {
+    year--;
+    month += 12;
+  }
+
+  if (day < 0) {
+    month--;
+    var lastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+    day += lastMonth.getDate();
+  }
+
   showyear.innerHTML = year;
   showmonth.innerHTML = month;
   showday.innerHTML = day;
